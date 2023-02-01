6e42ce05030885d73d128ceccc343b0d2d0485e8
Austin Erlandson <austinerlandson77@gmail.com>
2023-02-01 17:06:58 -0600

    Use blaze-htmx & cdns cause they're fast anyways.

--- Server.hs
+++ Server.hs
@@ -1,118 +1,107 @@
+{-# LANGUAGE BlockArguments #-}
 module Main (main) where
 
-import Control.Applicative ((<|>))
-import Control.Concurrent.Async (async, waitAny)
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai.Handler.WarpTLS as TLS
-import qualified Options.Applicative as OA
-import Servant
-  ( Get,
-    Proxy (Proxy),
-    Raw,
-    Server,
-    serve,
-    serveDirectoryWith,
-    (:<|>) ((:<|>)),
-    (:>),
-  )
-import Servant.HTML.Blaze (HTML)
-import System.FilePath.Posix (addTrailingPathSeparator)
-import System.IO
-  ( IOMode (AppendMode),
-    hPrint,
-    openFile,
-  )
-import qualified Text.Blaze as B
-import Text.Blaze.Html5 ((!))
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import WaiAppStatic.Storage.Filesystem (defaultWebAppSettings)
+import           Control.Applicative             ((<|>))
+import           Control.Concurrent.Async        (async, waitAny)
+import           Data.Foldable                   (traverse_)
+import           GHC.Generics                    (Generic)
+import qualified Network.Wai.Handler.Warp        as Warp
+import qualified Network.Wai.Handler.WarpTLS     as TLS
+import qualified Options.Applicative             as OA
+import           Servant
+import           Servant.HTML.Blaze              (HTML)
+import           System.FilePath.Posix           (addTrailingPathSeparator)
+import           System.IO                       (IOMode (AppendMode), hPrint,
+                                                  openFile)
+import qualified Text.Blaze.Html5                as H
+import           Text.Blaze.Html5                (Html, (!))
+import qualified Text.Blaze.Html5.Attributes     as A
+import           Text.Blaze.Htmx
+import           WaiAppStatic.Storage.Filesystem (defaultWebAppSettings)
 
-type Site =
-  -- pages at /smile and /wow
-  "smile" :> Get '[HTML] H.Html
-    :<|> "wow" :> Get '[HTML] H.Html
-    -- The index
-    :<|> Get '[HTML] H.Html
-    -- Any other page serves a file from content/
-    :<|> Raw
 
-site :: Proxy Site
-site = Proxy
+type Site = NamedRoutes Site_
 
-serveDirectory :: FilePath -> Server Raw
-serveDirectory = serveDirectoryWith . defaultWebAppSettings . addTrailingPathSeparator
+data Site_ r = Site_
+  { _index :: r :- Get '[HTML] Html
+  , _smile :: r :- "smile" :> Get '[HTML] Html
+  , _wow   :: r :- "wow" :> Get '[HTML] Html
+  , static :: r :- Raw
+  }
+  deriving Generic
 
-style :: H.Html
-style = H.style $ do
+serveDirectory_ :: FilePath -> Server Raw
+serveDirectory_ = serveDirectoryWith . defaultWebAppSettings . addTrailingPathSeparator
+
+style :: Html
+style = H.style do
   "body {background-color: linen;}"
   "p {color: maroon;}"
   ".buttonDiv {color: blue;}"
   "@font-face {font-family: 'Berkeley Mono'; src: url('/BerkeleyMono-Regular.woff2') format('woff2');}"
   "span.mono {font-family: 'Berkeley Mono';}"
 
-smile :: H.Html
-smile = H.div ! hxGet "/wow" ! outer ! A.class_ "buttonDiv" $ "Click me :)"
+smile :: Html
+smile = H.div ! hxGet "/wow" ! hxSwap "outerHTML" ! A.class_ "buttonDiv" $ "Click me :)"
 
-wow :: H.Html
-wow = H.div ! hxGet "/smile" ! outer ! A.class_ "buttonDiv" $ "Click me :o"
+wow :: Html
+wow = H.div ! hxGet "/smile" ! hxSwap "outerHTML" ! A.class_ "buttonDiv" $ "Click me :o"
 
-hxGet :: B.AttributeValue -> B.Attribute
-hxGet = B.customAttribute "hx-get"
 
-hxSwap :: B.AttributeValue -> B.Attribute
-hxSwap = B.customAttribute "hx-swap"
-
-outer :: B.Attribute
-outer = hxSwap "outerHTML"
-
-index :: H.Html
-index = H.body $ do
-  H.head $ do
+index :: Html
+index = H.body do
+  H.head do
     style
-    H.script ! A.src "/htmx.min.js" $ ""
-  H.body $ do
-    H.p $ do
+    H.script ! A.src "https://unpkg.com/htmx.org@1.8.5" $ ""
+    H.script ! A.src "https://cdn.tailwindcss.com" $ ""
+  H.body ! A.class_ "container sm mx-auto" $ do
+    H.p do
       "Welcome to "
       H.span ! A.class_ "mono" $ "<The Index>"
     smile
 
-makeServer :: FilePath -> IO (Server Site)
-makeServer contentPath = return server
-  where
-    files = serveDirectory contentPath
-    server = return smile :<|> return wow :<|> return index :<|> files
+makeServer :: FilePath -> Server Site
+makeServer contentPath =
+  Site_
+    { static = serveDirectory_ contentPath
+    , _index = pure index
+    , _smile = pure smile
+    , _wow = pure wow
+    }
 
 data TLSMode = NoTLS | TLS {tlsCertPath :: FilePath, tlsKeyPath :: FilePath, tlsPort :: Warp.Port}
 
 tlsModeArgs :: OA.Parser TLSMode
-tlsModeArgs = noTLS <|> (TLS <$> tlsCertPath <*> tlsKeyPath <*> tlsPort)
+tlsModeArgs =  (TLS <$> tlsCertPath <*> tlsKeyPath <*> tlsPort) <|> pure NoTLS
   where
-    noTLS = OA.flag' NoTLS (OA.long "no-tls")
     tlsPort = OA.option OA.auto (OA.long "tls-port" <> OA.value 443 <> OA.showDefault)
     tlsCertPath = OA.option OA.str (OA.long "tls-cert-path" <> OA.metavar "TLS_CERT_PATH")
     tlsKeyPath = OA.option OA.str (OA.long "tls-key-path" <> OA.metavar "TLS_KEY_PATH")
 
-data Command = Serve {logPath :: FilePath, contentPath :: FilePath, httpPort :: Warp.Port, tlsMode :: TLSMode}
+data Config = Config
+  { logPath     :: Maybe FilePath
+  , contentPath :: FilePath
+  , httpPort    :: Warp.Port
+  , tlsMode     :: TLSMode
+  }
 
-command :: OA.Parser Command
+command :: OA.Parser Config
 command = serveCmd
   where
-    serverSwitch = OA.flag' () (OA.long "server" <> OA.short 's')
-    logPath = OA.option OA.str (OA.long "log-path" <> OA.short 'l' <> OA.metavar "LOG_PATH")
-    contentPath = OA.option OA.str (OA.long "content-path" <> OA.short 'c' <> OA.metavar "CONTENT_PATH")
-    httpPort = OA.option OA.auto (OA.long "http-port" <> OA.value 80 <> OA.showDefault)
-    serveCmd = serverSwitch *> (Serve <$> logPath <*> contentPath <*> httpPort <*> tlsModeArgs)
+    logPath = OA.optional $ OA.option OA.str (OA.long "log-path" <> OA.short 'l' <> OA.metavar "LOG_PATH")
+    contentPath = OA.option OA.str (OA.long "content-path" <> OA.short 'c' <> OA.metavar "CONTENT_PATH" <> OA.value "content" <> OA.showDefault)
+    httpPort = OA.option OA.auto (OA.long "http-port" <> OA.value 7373 <> OA.showDefault)
+    serveCmd = Config <$> logPath <*> contentPath <*> httpPort <*> tlsModeArgs
 
 main :: IO ()
 main = do
   OA.execParser (OA.info (OA.helper <*> command) OA.fullDesc) >>= \case
-    Serve {..} -> do
-      logHandle <- openFile logPath AppendMode
-      server <- makeServer contentPath
-      let application = \req respond -> do
-            hPrint logHandle req
-            serve site server req respond
+    Config {..} -> do
+      logHandle <- traverse (`openFile` AppendMode) logPath
+      let server = makeServer contentPath
+          application req response = do
+            traverse_ (`hPrint` req) logHandle
+            serve (Proxy @Site) server req response
           http = Warp.runSettings (Warp.setPort httpPort Warp.defaultSettings) application
           https tlsSettings tlsPort = TLS.runTLS tlsSettings (Warp.setPort tlsPort Warp.defaultSettings) application
       case tlsMode of
